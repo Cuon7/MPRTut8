226e95002c028210852bf2736d20b29d8ff3ce3a
Cuon7 <74196060+Cuon7@users.noreply.github.com>
2021-03-27 17:26:43 +0700

    Add files via upload
    Beta testing

---
+++ app/src/main/java/com/a1_1801040028/mprtut8/MainActivity.java
@@ -0,0 +1,14 @@
+package com.a1_1801040028.mprtut8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
